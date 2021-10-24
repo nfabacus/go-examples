c5a4d105ab2aeb748588909a0c56d182651304ca
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2021-10-24 09:40:45 +0100

    update 12-basic-web-app with templates

--- 12-basic-web-app/cmd/web/main.go
+++ 12-basic-web-app/cmd/web/main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/nfabacus/myapp/pkg/config"
 	"github.com/nfabacus/myapp/pkg/handlers"
+	"github.com/nfabacus/myapp/pkg/render"
+	"log"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
